18733ba41aa244797107f85c0d04a8a9398011ff
Kornilios Kourtis <kornilios@isovalent.com>
2022-06-29 09:04:07 +0200

    pkg/logcmd: log command outputs
    Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

---
+++ pkg/logcmd/logcmd.go
@@ -0,0 +1,85 @@
+package logcmd
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os/exec"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+)
+
+func logReader(rd *bufio.Reader, log *logrus.Logger, prefix string, level logrus.Level) error {
+	for {
+		line, err := rd.ReadString('\n')
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+			return err
+		}
+
+		log.Logf(level, "%s%s", prefix, line)
+	}
+}
+
+func runAndLogCommand(
+	cmd *exec.Cmd,
+	log *logrus.Logger,
+	stdoutLevel, stderrLevel logrus.Level,
+) error {
+
+	// prepare pipes
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("StdErrPipe() failed: %w", err)
+	}
+	defer stderr.Close()
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("StdOutPipe() failed: %w", err)
+	}
+	defer stdout.Close()
+
+	// start command
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("failed to execute command: %w", err)
+	}
+
+	// start logging
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		rd := bufio.NewReader(stdout)
+		err = logReader(rd, log, "stdout> ", stdoutLevel)
+		if err != nil {
+			log.Warnf("failed to read from stdout: %v", err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		rd := bufio.NewReader(stderr)
+		err = logReader(rd, log, "stderr> ", stderrLevel)
+		if err != nil {
+			log.Warnf("failed to read from stderr: %v", err)
+		}
+	}()
+
+	// we need to wait for the pipes before waiting for the command
+	// see: https://pkg.go.dev/os/exec#Cmd.StdoutPipe
+	wg.Wait()
+
+	return cmd.Wait()
+}
+
+func RunAndLogCommand(
+	cmd *exec.Cmd,
+	log *logrus.Logger,
+) error {
+	return runAndLogCommand(cmd, log, logrus.WarnLevel, logrus.InfoLevel)
+}
---
+++ pkg/logcmd/logcmd_test.go
@@ -0,0 +1,40 @@
+package logcmd
+
+import (
+	"io/ioutil"
+	"os/exec"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+type logrusRecorder struct {
+	Level    logrus.Level
+	Messages []string
+}
+
+func (r *logrusRecorder) Levels() []logrus.Level {
+	return []logrus.Level{r.Level}
+}
+
+func (r *logrusRecorder) Fire(e *logrus.Entry) error {
+	r.Messages = append(r.Messages, e.Message)
+	return nil
+}
+
+func TestLogcmd(t *testing.T) {
+	cmd := exec.Command("/bin/sh", "-c", "echo FOO>&2; echo LALA")
+	log := logrus.New()
+	log.SetOutput(ioutil.Discard)
+
+	infoRecorder := logrusRecorder{Level: logrus.InfoLevel}
+	warnRecorder := logrusRecorder{Level: logrus.WarnLevel}
+	log.AddHook(&infoRecorder)
+	log.AddHook(&warnRecorder)
+
+	err := runAndLogCommand(cmd, log, logrus.InfoLevel, logrus.WarnLevel)
+	assert.Nil(t, err)
+	assert.Equal(t, []string{"stderr> FOO\n"}, warnRecorder.Messages)
+	assert.Equal(t, []string{"stdout> LALA\n"}, infoRecorder.Messages)
+}
