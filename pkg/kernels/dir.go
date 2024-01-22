cilium/little-vm-helper

commit 06c67d1fbc582ffd6f5b76211d296ac494c27af9

kernels: add a extra_make_args parameter
This can be used for adding extra arguments to make, as it is used to
build kernels.

For example:
```
{
    "name": "4.19",
    "url": "git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git?depth=1#linux-4.19.y"
    "extra_make_args": [
	    "V=1",
	    "HOSTCC=gcc-10",
	    "CC=gcc-10"
    ]
}
```

Signed-off-by: Kornilios Kourtis <kornilios@isovalent.com>

pkg/kernels/dir.go
@@ -129,6 +129,18 @@ func kcfonfigValidate(opts []ConfigOption) error {
 	return ret
 }
 
+func runAndLogMake(
+	ctx context.Context,
+	log *logrus.Logger,
+	kc *KernelConf,
+	makeArgs ...string,
+) error {
+	if len(kc.ExtraMakeArgs) > 0 {
+		makeArgs = append(makeArgs, kc.ExtraMakeArgs...)
+	}
+	return logcmd.RunAndLogCommandContext(ctx, log, MakeBinary, makeArgs...)
+}
+
 func (kd *KernelsDir) configureKernel(ctx context.Context, log *logrus.Logger, kc *KernelConf) error {
 	srcDir := filepath.Join(kd.Dir, kc.Name)
 
@@ -144,7 +156,7 @@ func (kd *KernelsDir) configureKernel(ctx context.Context, log *logrus.Logger, k
 
 	configOptions := kd.Conf.getOptions(kc)
 
-	if err := logcmd.RunAndLogCommandContext(ctx, log, MakeBinary, "defconfig", "prepare"); err != nil {
+	if err := runAndLogMake(ctx, log, kc, "defconfig", "prepare"); err != nil {
 		return err
 	}
 
@@ -170,7 +182,7 @@ func (kd *KernelsDir) configureKernel(ctx context.Context, log *logrus.Logger, k
 	}
 
 	// run make olddefconfig to clean up the config file, and ensure that everything is in order
-	if err := logcmd.RunAndLogCommandContext(ctx, log, MakeBinary, "olddefconfig"); err != nil {
+	if err := runAndLogMake(ctx, log, kc, "olddefconfig"); err != nil {
 		return err
 	}
 
@@ -203,11 +215,12 @@ func (kd *KernelsDir) buildKernel(ctx context.Context, log *logrus.Logger, kc *K
 	}
 
 	ncpus := fmt.Sprintf("%d", runtime.NumCPU())
-	if err := logcmd.RunAndLogCommandContext(ctx, log, MakeBinary, "-C", srcDir, "-j", ncpus, "bzImage", "modules"); err != nil {
+	ncpus = "1"
+	if err := runAndLogMake(ctx, log, kc, "-C", srcDir, "-j", ncpus, "bzImage", "modules"); err != nil {
 		return fmt.Errorf("buiding bzImage && modules failed: %w", err)
 	}
 
-	if err := logcmd.RunAndLogCommandContext(ctx, log, MakeBinary, "-C", srcDir, "tar-pkg"); err != nil {
+	if err := runAndLogMake(ctx, log, kc, "-C", srcDir, "tar-pkg"); err != nil {
 		return fmt.Errorf("build dir failed: %w", err)
 	}
 

pkg/kernels/conf.go
@@ -23,6 +23,8 @@ type KernelConf struct {
 	URL string `json:"url"`
 	// config options
 	Opts []ConfigOption `json:"opts,omitempty"`
+	// Extra make args
+	ExtraMakeArgs []string `json:"extra_make_args,omitempty"`
 }
 
 type Conf struct {

commit ef2264417bde01ca7c0de78aaebaaf2e6d4d474c

kernel/build: fix ncpu detection
Commit 06c67d1 broke ncpu detection by overriding the value with 1 always. This makes it impossible to build kernels quickly without manually overriding make args for each kernel in kernels.json, which is not ideal in my opinion. Moreover, this looks like a change that was inadvertently introduced during testing given that the line which properly initializes ncpus was left in immediately above. Revert to the old behaviour.

Signed-off-by: William Findlay <will@isovalent.com>

pkg/kernels/dir.go
@@ -215,7 +215,6 @@ func (kd *KernelsDir) buildKernel(ctx context.Context, log *logrus.Logger, kc *K
 	}
 
 	ncpus := fmt.Sprintf("%d", runtime.NumCPU())
-	ncpus = "1"
 	if err := runAndLogMake(ctx, log, kc, "-C", srcDir, "-j", ncpus, "bzImage", "modules"); err != nil {
 		return fmt.Errorf("buiding bzImage && modules failed: %w", err)
 	}