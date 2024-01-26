cilium/little-vm-helper

commit ea09cb7b4e952af4bd20177cf4c4c5575567f364

images: Add docker pull support
Add support to pull LVH images directly from the commandline so that
users who don't need to customize the image can directly pull the
required images directly rather than having to run another script or
manually pull & decompress the images locally.

Example usage:

$ lvh images pull quay.io/lvh-images/kind:5.4-main

Signed-off-by: Joe Stringer <joe@isovalent.com>

pkg/images/pull.go
@@ -1,0 +1,145 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package images
+
+import (
+	"archive/tar"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+)
+
+type PullConf struct {
+	Image     string
+	TargetDir string
+	Cache     bool
+}
+
+type PullResult struct {
+	Images []string
+}
+
+// PullImage pulls an OCI image from a remote repository and decompresses it
+// into a local directory.
+func PullImage(ctx context.Context, conf PullConf) (*PullResult, error) {
+	result := &PullResult{
+		Images: make([]string, 0, 1),
+	}
+
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, fmt.Errorf("cannot establish client for image %s: %w", conf.Image, err)
+	}
+	defer cli.Close()
+
+	remotePullReader, err := cli.ImagePull(ctx, conf.Image, types.ImagePullOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("cannot pull image %s: %w", conf.Image, err)
+	}
+	defer remotePullReader.Close()
+
+	// Complete the image pull
+	// TODO: Take the output and overwrite each line in stdout for pretty
+	//       status output instead of spamming the terminal with json
+	io.Copy(os.Stdout, remotePullReader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: conf.Image,
+		Tty:   false,
+	}, nil, nil, nil, "")
+	if err != nil {
+		return nil, fmt.Errorf("cannot create container from %s: %w", conf.Image, err)
+	}
+	defer func() {
+		err := cli.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{
+			Force: true,
+		})
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not clean up container %s: %s\n", resp.ID, err)
+		}
+	}()
+
+	ctrImagesPath := filepath.Join("/", "data", "images")
+	imageReader, _, err := cli.CopyFromContainer(ctx, resp.ID, ctrImagesPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to locate images inside %s: %w", conf.Image, err)
+	}
+	defer imageReader.Close()
+
+	tarReader := tar.NewReader(imageReader)
+	for {
+		hdr, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to read tar header in %s: %w", conf.Image, err)
+		}
+
+		image, err := handleTarObject(ctx, tarReader, hdr, conf, resp.ID)
+		if err != nil {
+			return nil, err
+		}
+		if image != "" {
+			result.Images = append(result.Images, image)
+		}
+	}
+
+	return result, nil
+}
+
+func handleTarObject(ctx context.Context, tr *tar.Reader, hdr *tar.Header, conf PullConf, containerID string) (string, error) {
+	image := ""
+
+	dstPath := filepath.Join(conf.TargetDir, hdr.Name)
+	switch hdr.Typeflag {
+	case tar.TypeDir:
+		if err := os.MkdirAll(dstPath, 0755); err != nil {
+			return image, fmt.Errorf("failed to create directory %s: %w", dstPath, err)
+		}
+	case tar.TypeReg:
+		compressed := strings.HasSuffix(dstPath, ".zst")
+		if compressed {
+			image = strings.TrimSuffix(dstPath, ".zst")
+			cmd := exec.CommandContext(ctx, "zstd", "-d", "-", "-o", image)
+			cmd.Stdin = tr
+
+			if _, err := cmd.Output(); err != nil {
+				var e *exec.ExitError
+				if errors.As(err, &e) {
+					fmt.Fprintf(os.Stderr, string(e.Stderr))
+				}
+				return image, fmt.Errorf("failed during zst decompression of %s: %w", hdr.Name, err)
+			}
+		}
+		if conf.Cache || !compressed {
+			dst, err := os.Create(dstPath)
+			if err != nil {
+				return image, fmt.Errorf("failed to create file %s: %w", dstPath, err)
+			}
+			defer dst.Close()
+
+			n, err := io.CopyN(dst, tr, hdr.Size)
+			if err != nil {
+				return image, fmt.Errorf("failed to copy %s from container %s: %w", dstPath, containerID, err)
+			}
+			if n != hdr.Size {
+				return image, fmt.Errorf("tar header reports file %s size %d, but only %d bytes were pulled", hdr.Name, hdr.Size, n)
+			}
+		}
+	default:
+		return image, fmt.Errorf("unexpected tar header type %d", hdr.Typeflag)
+	}
+
+	return image, nil
+}

cmd/lvh/images/pull.go
@@ -1,0 +1,45 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package images
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/cilium/little-vm-helper/pkg/images"
+	"github.com/spf13/cobra"
+)
+
+var (
+	dirName string
+	cache   bool
+)
+
+func PullCmd() *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:        "pull <URL>",
+		Short:      "Pull an image from an OCI repository",
+		Args:       cobra.MinimumNArgs(1),
+		ArgAliases: []string{"imageURL"},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := os.MkdirAll(dirName, 0755); err != nil {
+				return fmt.Errorf("failed to create directory %s: %w", dirName, err)
+			}
+
+			_, err := images.PullImage(context.Background(), images.PullConf{
+				Image:     args[0],
+				TargetDir: dirName,
+				Cache:     cache,
+			})
+			return err
+		},
+	}
+
+	cmd.Flags().StringVar(&dirName, "dir", "_data", "directory to keep the images (images will be saved in images in <dir>/images)")
+	cmd.Flags().BoolVar(&cache, "cache", false, "cache a compressed version of the image")
+
+	return cmd
+}

cmd/lvh/images/images.go
@@ -13,6 +13,6 @@ func ImagesCommand() *cobra.Command {
 		Short: "Build VM images",
 	}
 
-	ret.AddCommand(BuildCmd(), ExampleCmd())
+	ret.AddCommand(BuildCmd(), ExampleCmd(), PullCmd())
 	return ret
 }

scripts/pull_image.sh
@@ -1,28 +1,0 @@
-#!/usr/bin/env bash
-
-CTR=""
-IMAGES_DIR="${IMAGES_DIR:-.}"
-
-cleanup() {
-    docker rm $CTR
-}
-
-# $1 - image
-main() {
-    if [ $# -ne 1 ]; then
-        >&2 echo "usage: $0 <quay.io/lvh-images/kind:TAG>"
-        exit 1
-    fi
-
-    mkdir -p $IMAGES_DIR
-    CTR=$(docker create $1)
-    trap cleanup EXIT
-    docker cp $CTR:/data/images $IMAGES_DIR >/dev/null
-
-    files=($(find $IMAGES_DIR/images/*.zst -type f))
-    for f in ${files[@]}; do
-        zstd --decompress $f
-    done
-}
-
-main "$@"

commit 1dfca6084fbe34c774ea8f35847e893e13b2e632

images: Fix read past end of file for pull --cache
Pull has two modes:
- No cache: Extract and decompress the target VM image from the docker
  filesystem to the host filesystem. Result: One file, image.qcow.
- Cache: Same as no cache, but also copy the compressed image to the
  host filesystem. Result: two files, image.qcow{,.zst}

Previously, the logic for extracting the target VM image from the docker
image with --cache enabled was to:
- Extract and decompress the target file directly from inside the docker
  image filesystem to the host filesystem
- Do not seek the source Reader back to the start of the file
- Attempt (and fail) to copy the entire compressed image also to the
  host filesystem, but starting from the end of the source file.
- This resulted in an EOF error because the reader wasn't reset to start
  from the beginning.

Unfortunately, tar.Reader doesn't expose a way to seek back to the start
of the source file. We could perhaps get an additional reference to a
reader starting from the beginning of the current tar header in order to
reset back to the beginning of the target file in cache mode, but this
was getting a bit complicated.

The solution in this patch is to:
- Copy the compressed file to a temporary path on the host filesystem
- Extract the compressed file from this temporary host-side cache to the
  intended destination directory.
- If in cache mode, move the temporary file also to the output
  directory. Otherwise, delete the temporary file.

There's one minor extra case, if the qcow inside the docker image is not
compressed at all then we skip decompression, and in that case we also
need to move the temporary path to the destination directory.

Fixes: ea09cb7b4e95 ("images: Add docker pull support")
Reported-by: Lorenz Bauer <lmb@isovalent.com>
Signed-off-by: Joe Stringer <joe@isovalent.com>

pkg/images/pull.go
@@ -5,6 +5,7 @@ package images
 
 import (
 	"archive/tar"
+	"bufio"
 	"context"
 	"errors"
 	"fmt"
@@ -121,41 +122,66 @@ func handleTarObject(ctx context.Context, tr *tar.Reader, hdr *tar.Header, conf 
 		}
 	case tar.TypeReg:
 		compressed := strings.HasSuffix(dstPath, ".zst")
+
+		// Copy the target file out to the host (compressed or not).
+		// On failure, clean up the temporary file; otherwise move it
+		// to the target destination.
+		tmpFile, err := os.CreateTemp("", filepath.Base(hdr.Name))
+		if err != nil {
+			return image, fmt.Errorf("failed to open temporary file for %s: %w", hdr.Name, err)
+		}
+		defer func() {
+			tmpPath := tmpFile.Name()
+			tmpFile.Close()
+			if err != nil || (!conf.Cache && compressed) {
+				os.Remove(tmpPath)
+				return
+			}
+			if err = os.Rename(tmpFile.Name(), dstPath); err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to move %s to %s: %s", tmpPath, dstPath, err)
+			}
+		}()
+
+		n, err := io.CopyN(tmpFile, tr, hdr.Size)
+		if err != nil {
+			return image, fmt.Errorf("failed to copy %s from container %s: %w", dstPath, containerID, err)
+		}
+		if n != hdr.Size {
+			return image, fmt.Errorf("tar header reports file %s size %d, but only %d bytes were pulled", hdr.Name, hdr.Size, n)
+		}
+
 		if compressed {
-			image = strings.TrimSuffix(dstPath, ".zst")
-			if _, err := os.Stat(image); err == nil {
-				return image, os.ErrExist
+			if _, err = tmpFile.Seek(0, 0); err != nil {
+				return image, fmt.Errorf("cannot seek to the start of the compressed target file %s: %w", dstPath, err)
 			}
+			compressedTarget := bufio.NewReader(tmpFile)
+			dstImagePath := strings.TrimSuffix(dstPath, ".zst")
 
-			cmd := exec.CommandContext(ctx, "zstd", "-d", "-", "-o", image)
-			cmd.Stdin = tr
-
-			if _, err := cmd.Output(); err != nil {
-				var e *exec.ExitError
-				if errors.As(err, &e) {
-					fmt.Fprintf(os.Stderr, string(e.Stderr))
-				}
-				return image, fmt.Errorf("failed during zst decompression of %s: %w", hdr.Name, err)
-			}
+			return dstImagePath, extractZst(ctx, compressedTarget, dstImagePath)
 		}
-		if conf.Cache || !compressed {
-			dst, err := os.Create(dstPath)
-			if err != nil {
-				return image, fmt.Errorf("failed to create file %s: %w", dstPath, err)
-			}
-			defer dst.Close()
 
-			n, err := io.CopyN(dst, tr, hdr.Size)
-			if err != nil {
-				return image, fmt.Errorf("failed to copy %s from container %s: %w", dstPath, containerID, err)
-			}
-			if n != hdr.Size {
-				return image, fmt.Errorf("tar header reports file %s size %d, but only %d bytes were pulled", hdr.Name, hdr.Size, n)
-			}
-		}
 	default:
 		return image, fmt.Errorf("unexpected tar header type %d", hdr.Typeflag)
 	}
 
 	return image, nil
+}
+
+func extractZst(ctx context.Context, reader io.Reader, dstPath string) error {
+	if _, err := os.Stat(dstPath); err == nil {
+		return os.ErrExist
+	}
+
+	cmd := exec.CommandContext(ctx, "zstd", "-d", "-", "-o", dstPath)
+	cmd.Stdin = reader
+
+	if _, err := cmd.Output(); err != nil {
+		var e *exec.ExitError
+		if errors.As(err, &e) {
+			os.Stderr.Write(e.Stderr)
+		}
+		return fmt.Errorf("failed during zst decompression to %s: %w", dstPath, err)
+	}
+
+	return nil
 }